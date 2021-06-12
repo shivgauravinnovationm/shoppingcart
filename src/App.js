66538d0ac297986a22e89d5c5d6c04402c5e6c55
shivgaurav <shiv.gaurav@innovationm.com>
2021-06-12 08:39:41 +0530

    routing and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Login from './components/Login';
 import Sidebar from './components/Sidebar';
-
+import Home from "./components/pages/Home"
 function App() {
   return (
     <div>
       <Header />
+      <Sidebar />
+      <Home />
 
     </div>
   );
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Sidebar from "../../Sidebar"
+
 function Home() {
     return (
-        <div className="position-relative">
-            <Sidebar />
-            {/* <h1>home component</h1> */}
+        <div className="position-relative p-2" style={{ marginLeft: "230px" }}>
+            <h1>home component</h1>
 
         </div>
     )
